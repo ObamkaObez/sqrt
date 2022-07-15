31f55ab03a3a911c592cfc743b0e9683f4ce7283
ObamkaObez <106184524+ObamkaObez@users.noreply.github.com>
2022-07-15 16:15:33 +0300

    Add files via upload

---
+++ sqrtX/sqrtX/sqrtX.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+
+int main() {
+    long double x, eps, i = 1;
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+        in >> x >> eps;
+        long double y0 = x;
+        long double y1 = (0.5) * (y0 + x / y0);
+        while (fabs(y1 - y0) > eps)
+        {
+            y0 = y1;
+            y1 = (0.5) * (y0 + x / y0);
+        }
+        out << setprecision(10) << y1;
+        out.close();
+    return 0;
+}
